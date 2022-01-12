e0dff59690ddef696e97a298686c46c7bb31d86b
Nur <nurmohammed840@gmail.com>
2022-01-12 17:20:29 +0600

    init

---
+++ README.md
@@ -0,0 +1,55 @@
+#[Docs](https://docs.rs/page-lock)
+
+An async library for locking page address.
+
+You may want to use this library to lock spacific page address. (For example database)
+
+#### Example
+
+```rust
+use page_lock::PageLocker;
+use std::sync::Arc;
+use std::time::Duration;
+use tokio::{spawn, time::sleep};
+
+let locker = Arc::new(PageLocker::new());
+let locker1 = locker.clone();
+tokio::try_join!(
+    spawn(async move {
+        let _lock = locker.lock(1).await;
+        println!("(1) Page 1: Locked");
+        sleep(Duration::from_secs(3)).await;
+        println!("(3) Page 1: Droping lock");
+    }),
+    spawn(async move {
+        println!("(2) Page 1: Waiting for unlock...");
+        locker1.unlock(1).await;
+        println!("(4) Page 1: Unlocked!");
+    })
+)
+.unwrap();
+```
+
+### Locking mechanism
+
+You may think something like this: `Map<Number, Mutex>`. where `Number` is page address.
+
+Unlocking method: `unlock(Number)` does:
+
+```
+function Unlock(Number):
+    if there is any `Mutex` in `Map` by `Number`:
+        wait for it, to get removed. 
+```
+
+locking method is `lock(Number)`,
+
+```
+function Lock(Number):
+    Unlock(Number)
+    
+    add a `Mutex` to `Map` by `Number`
+
+    return a `LockGuard`:
+        when `LockGuard` is dropped, remove the `Mutex` from `Map` by `Number`.
+```
---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+use std::{
+    collections::HashMap,
+    future::Future,
+    hash::Hash,
+    pin::Pin,
+    sync::RwLock,
+    task::{Context, Poll, Waker},
+};
+
+type Locker<T> = RwLock<HashMap<T, Vec<Waker>>>;
+
+pub struct UnLock<'a, T> {
+    num:    T,
+    state:  bool,
+    locker: &'a Locker<T>,
+}
+
+impl<'a, T: Unpin + Eq + Hash> Future for UnLock<'a, T> {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.state {
+            return Poll::Ready(());
+        }
+        let this = self.get_mut();
+        this.state = true;
+        this.locker.write().unwrap().get_mut(&this.num).unwrap().push(cx.waker().clone());
+        Poll::Pending
+    }
+}
+
+pub struct LockGuard<'a, T: Eq + Hash> {
+    num:    T,
+    locker: &'a Locker<T>,
+}
+
+impl<T: Eq + Hash> Drop for LockGuard<'_, T> {
+    fn drop(&mut self) {
+        for waker in self.locker.write().unwrap().remove(&self.num).unwrap() {
+            waker.wake();
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct PageLocker<T> {
+    locker: Locker<T>,
+}
+
+impl<T: Eq + Hash + Copy + Unpin> PageLocker<T> {
+    pub fn new() -> Self { Self { locker: RwLock::new(HashMap::new()) } }
+
+    #[inline(always)]
+    pub fn unlock(&self, num: T) -> UnLock<T> {
+        UnLock { state: self.locker.read().unwrap().get(&num).is_none(), locker: &self.locker, num }
+    }
+
+    #[inline(always)]
+    pub async fn lock(&self, num: T) -> LockGuard<'_, T> {
+        self.unlock(num).await;
+        self.locker.write().unwrap().insert(num, Vec::new());
+        LockGuard { num, locker: &self.locker }
+    }
+}
